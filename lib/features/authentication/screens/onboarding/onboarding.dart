d17c5eea714b9278e893dc4846db75fceba66ec5
ngb4o <ngbao08052003@gmail.com>
2024-11-01 20:34:53 +0700

    onboarding screen

---
+++ lib/features/authentication/screens/onboarding/onboarding.dart
@@ -0,0 +1,39 @@
+part of 'onboarding_imports.dart';
+
+class OnBoardingScreen extends StatelessWidget {
+  const OnBoardingScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          PageView(
+            children: const [
+              // OnBoardingPage1
+              OnBoardingPage(
+                image: TImages.onBoardingImage1,
+                title: TTexts.onBoardingTitle1,
+                subTitle: TTexts.onBoardingSubTitle1,
+              ),
+
+              // OnBoardingPage2
+              OnBoardingPage(
+                image: TImages.onBoardingImage2,
+                title: TTexts.onBoardingTitle2,
+                subTitle: TTexts.onBoardingSubTitle2,
+              ),
+
+              // OnBoardingPage3
+              OnBoardingPage(
+                image: TImages.onBoardingImage3,
+                title: TTexts.onBoardingTitle3,
+                subTitle: TTexts.onBoardingSubTitle3,
+              )
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/features/authentication/screens/onboarding/onboarding_imports.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+import 'package:ve_amor_app/features/authentication/screens/onboarding/widgets/widget_imports.dart';
+import 'package:ve_amor_app/utils/constants/image_strings.dart';
+import 'package:ve_amor_app/utils/constants/text_strings.dart';
+part 'onboarding.dart';
