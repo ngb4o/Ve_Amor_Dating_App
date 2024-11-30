7f18d5172c2e19d0ea3c31ca4a86971c42d84feb
haoking1111 <ngbao08052003@gmail.com>
2024-11-30 16:26:40 +0700

    fix error camera

--- lib/features/authentication/screens/ initial_information/initial_information_imports.dart
+++ lib/features/authentication/screens/ initial_information/initial_information_imports.dart
@@ -1,4 +1,3 @@
-import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_barcode_scanner/flutter_barcode_scanner.dart';
