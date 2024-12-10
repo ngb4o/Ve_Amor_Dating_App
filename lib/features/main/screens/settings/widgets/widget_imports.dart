2f542c25a396ad8e05c265bcc5085aa57dbf420e
Hà Nguyễn Ngọc Huy <neetyuuhaku1202@gmail.com>
2024-12-11 01:28:13 +0700

    lam phan giao dien payment va card detail cho dep hon hihihi

--- lib/features/main/screens/settings/widgets/widget_imports.dart
+++ lib/features/main/screens/settings/widgets/widget_imports.dart
@@ -1,7 +1,19 @@
+library widget_imports;
+
 import 'package:flutter/material.dart';
 import 'package:ve_amor_app/utils/constants/colors.dart';
 import 'package:ve_amor_app/utils/helpers/helper_functions.dart';
 import 'package:get/get.dart';
+import 'package:ve_amor_app/features/personalization/controller/user_controller.dart';
+import 'package:ve_amor_app/features/personalization/models/user_model.dart';
+import 'package:ve_amor_app/features/personalization/controller/remove_account/remove_account_controller.dart';
+import 'package:ve_amor_app/features/personalization/screens/profile/profile_photo/profile_photo.dart';
+import 'package:ve_amor_app/features/personalization/screens/profile/update_profile/update_imports.dart';
+import 'package:ve_amor_app/features/personalization/screens/profile/widgets/profile_bottom_sheet.dart';
+import 'package:ve_amor_app/features/personalization/screens/profile/widgets/widget_imports.dart';
+import 'package:ve_amor_app/utils/helpers/helper_functions.dart';
+
 
 part 'setting_upgrade_card.dart';
 part 'setting_upgrade_card_detail.dart';
+part 'payment.dart';
